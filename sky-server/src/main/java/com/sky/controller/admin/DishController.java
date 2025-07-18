c9c9e1dd86e6b8f6e1e92ef5628ddc6ae376dcf0
谁解幽人幽意 <2634118472@qq.com>
2025-07-18 15:46:21 +0800

    建立菜品功能管理的服务

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,15 @@
+package com.sky.controller.admin;
+
+
+import com.sky.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/dish")
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sky.service.impl;
+
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+
+}
