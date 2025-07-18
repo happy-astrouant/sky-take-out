2b9645bf825f8d3b7bdb83d73796eb7976faeedc
谁解幽人幽意 <2634118472@qq.com>
2025-07-18 11:22:12 +0800

    默认result.msg为null

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -18,7 +18,6 @@ public class Result<T> implements Serializable {
     public static <T> Result<T> success() {
         Result<T> result = new Result<T>();
         result.code = 1;
-        result.msg = "";
         return result;
     }
 
