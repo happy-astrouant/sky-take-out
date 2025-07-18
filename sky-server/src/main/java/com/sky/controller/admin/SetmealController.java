d57b4ade4fc4d85e161e066c0d90186a6f882da7
谁解幽人幽意 <2634118472@qq.com>
2025-07-18 14:32:17 +0800

    定义套餐相关的接口和类

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,14 @@
+package com.sky.controller.admin;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("admin/setmeal")
+public class SetmealController {
+
+
+}
