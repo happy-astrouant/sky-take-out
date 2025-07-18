88a1dddc36c6e1fb4257463c516d68bb76c815ca
谁解幽人幽意 <2634118472@qq.com>
2025-07-18 14:55:57 +0800

    添加AutoFill注解

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,7 +1,12 @@
 package com.sky.controller.admin;
 
 
+import com.sky.dto.SetmealDTO;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("admin/setmeal")
 public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /*
+    * 修改套餐信息
+    * */
+    @PutMapping
+    public Result update(SetmealDTO setmealDTO) {
+        log.info("修改套餐内容：{}", setmealDTO);
+        setmealService.update(setmealDTO);
+        return Result.success();
+    }
 
 
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
 
+import com.sky.entity.Setmeal;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SetmealMapper {
+
+    void update(Setmeal setmeal);
 }
