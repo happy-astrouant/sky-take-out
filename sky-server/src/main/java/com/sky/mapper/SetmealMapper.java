f5635c627df6c705830dbac97676add4cc308ab6
谁解幽人幽意 <2634118472@qq.com>
2025-07-18 19:44:05 +0800

    完成修改套餐内容的功能

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,15 +1,28 @@
 package com.sky.mapper;
 
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
 
     void update(Setmeal setmeal);
 
+    @AutoFill(value = OperationType.UPDATE)
     @Select("select count(1) from setmeal where category_id = #{categoryId}")
     int countByCategoryId(Long categoryId);
+
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long seatmealId);
+
+
+    void insertSetmealDish(List<SetmealDish> setmealDishes);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.context.BaseContext;
 import com.sky.dto.SetmealDTO;
 import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealMapper;
 import com.sky.service.SetmealService;
 import org.springframework.beans.BeanUtils;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class SetmealServiceImpl implements SetmealService {
@@ -21,8 +23,16 @@ public class SetmealServiceImpl implements SetmealService {
     public void update(SetmealDTO setmealDTO) {
         Setmeal setmeal = new Setmeal();
         BeanUtils.copyProperties(setmealDTO, setmeal);
-        setmeal.setUpdateTime(LocalDateTime.now());
-        setmeal.setUpdateUser(BaseContext.getCurrentId());
+        // 更新套餐表
         setmealMapper.update(setmeal);
+        // 删除套餐和菜品关联表数据
+        setmealMapper.deleteBySetmealId(setmealDTO.getId());
+        // 重新插入套餐和菜品的关联数据
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        if(setmealDishes != null && !setmealDishes.isEmpty()){
+            setmealDishes.forEach(dish -> dish.setSetmealId(setmealDTO.getId()));
+            setmealMapper.insertSetmealDish(setmealDishes);
+        }
+
     }
 }
